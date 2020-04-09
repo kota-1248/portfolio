4f2a53d2a57a919dbadab0c4a2271c3012369f4c
kota-1248 <ozawakouta@ozawakoufutoshinoMacBook-Pro.local>
2020-04-09 17:30:31 +0900

    [ポートフォリオ実装)2020MMDD

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,3 +1,86 @@
 <template>
-  <h1>K.Ozawa</h1>
+ <div id="mainSection">
+    <div id="userName">
+        K.ozawa
+    </div>
+    <div id="companyName">
+        Seattleconsulting
+    </div>
+    <img id="profilePicture" src="../assets/sample.jpg" >
+    <p><img src="sample.jpg" alt="写真" width="50px" height="30px"></p>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+#mainSection {
+    background: linear-gradient(180deg, #80E1F7, #06C1E6);
+    padding-bottom: 30px;
+    text-align: center;
+}
+#userName {
+    color: #FFFFFF;
+    font-family: 'Noto Suns';
+    font-weight: bold;
+    font-size: 18pt;
+    text-shadow: 0px 0.5px 2px black;
+    padding: 20px 0;
+    border-bottom: 1px solid#FFFFFF;
+    margin: 0 310pt;
+}
+#companyName {
+    padding-top: 20px;
+    padding-bottom: 20px;
+    color: #FFFFFF;
+    font-family: 'Noto Suns';
+    font-weight: bold;
+    font-size: 18pt;
+    text-shadow: 0px 0.5px 2px black;
+}
+h1{
+    color: white;
+    font-family: "Noto Sans";
+    font-weight:font-wight hold;
+    font-size:  18pt;
+    text-shadow: 1px 2px 3px #c0c0c0;
+}
+
+.body{
+  background: linear-gradient(#80E1F7,#06C1E6);  
+}
+
+hr{
+  width: 120pt;
+  color:white;
+  size:1px;
+}
+
+#profilePicture {
+    border-radius: 50%;
+    width: 64px;
+    height: 64px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+
+
+</style>
