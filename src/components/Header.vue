ae511c9507458d33b35160743cdc05a904686bf2
kota-1248 <ozawakouta@ozawakoufutoshinoMacBook-Pro.local>
2020-04-10 14:40:39 +0900

    【Lintの自動設定の追加】20200410

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
-    <button @click="toggle" class="btn btn-success">toggle</button>
-    <Drawer @close="toggle" align="left" :closeable="true">
-      <div v-if="open"></div>
+    <button
+      class="btn btn-success"
+      @click="toggle"
+    >
+      toggle
+    </button>
+    <Drawer
+      align="left"
+      :closeable="true"
+      @close="toggle"
+    >
+      <div v-if="open" />
     </Drawer>
     <header>
-  <div id="nav-drawer">
-      <input id="nav-input" type="checkbox" class="nav-unshown">
-      <label id="nav-open" for="nav-input"><span></span></label>
-      <label class="nav-unshown" id="nav-close" for="nav-input"></label>
-      <div id="nav-content">ここに中身を入れる</div>
-  </div>
-</header>
+      <div id="nav-drawer">
+        <input
+          id="nav-input"
+          type="checkbox"
+          class="nav-unshown"
+        >
+        <label
+          id="nav-open"
+          for="nav-input"
+        ><span /></label>
+        <label
+          id="nav-close"
+          class="nav-unshown"
+          for="nav-input"
+        />
+        <div id="nav-content">
+          ここに中身を入れる
+        </div>
+      </div>
+    </header>
   </div>
 </template>
  
@@ -37,7 +59,7 @@ export default {
 </script>
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap-reboot",
-"~bootstrap/scss/buttons";
+  "~bootstrap/scss/buttons";
 </style>
 <style>
 #app {
