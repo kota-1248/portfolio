cdf0b1a16ac7c4a47666156510eca761a43ae17e
kotaozawa <kota.ozawa@seattleconsulting.co.jp>
2020-04-20 18:23:13 +0900

    a

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/reset.css'
 import smoothScroll from 'vue-smoothscroll'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 Vue.use(smoothScroll)
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// npm install --save axios vue-axios を実行してデータにアクセスできるようにする
+
+
